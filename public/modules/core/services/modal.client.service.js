f766d65a4becc173e181c36d9de32b70c81e304b
Juan Manuel Sanchez <juan.sanchez@viapool.com>
2014-06-29 16:14:01 -0300

    upgrade modal service

---
+++ public/modules/core/services/modal.client.service.js
@@ -0,0 +1,44 @@
+
+'use strict';
+
+var defaultModalInstanceCtrl = function ($scope, $modalInstance, items) {
+
+  $scope.items = items;
+  $scope.selected = {
+    item: $scope.items[0]
+  };
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+};
+
+angular.module('core').factory('openModal', [ '$modal', function($modal) {
+	return function (next, scopeItems, modaCtrl, size)
+	{
+		if (typeof(scopeItems) === 'undefined') scopeItems = [];
+		if (typeof(modaCtrl)   === 'undefined') modaCtrl = defaultModalInstanceCtrl;
+
+		var modalInstance = $modal.open(
+		{
+			templateUrl: 'myModalContent.html',
+			controller: modaCtrl,
+			size: size,
+			resolve:{
+				items: function ()
+				{
+					return scopeItems;
+				}
+			}
+		});
+
+		modalInstance.result.then(function (selectedItem) {
+			next(selectedItem);
+		});
+	};	
+}]);
+
