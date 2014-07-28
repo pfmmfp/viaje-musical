99f5b5d911fcca8776a6034e1dd466964d52e13e
Pablo Morra <morrapablo@scvsoft.com>
2014-07-28 13:12:40 -0300

    Agregue los botones de navegacion. Tambien mejore los calculos de la grilla de muestras para que ande bien en distintas resoluciones.

---
+++ public/modules/composer/services/gridhelper.client.service.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('composer').factory('GridHelper', [
+	function() {
+
+		return {
+      beatSize: 25, //px
+      beats: 70,
+      snappedPosition: function(pos) {
+      	return Math.floor(pos / this.beatSize) * this.beatSize;
+      },
+      indexPosition: function(pos, instrument, delta) {
+      	return Math.floor(
+      		(
+	      		pos - 
+	      		this.offset(instrument).left + 
+	      		(delta ? this.originDelta(instrument) : 0)
+      		) / this.beatSize
+    		);
+      },
+      offset: function(instrument) {
+      	return angular.element('.track-grid.' + instrument + ' .track-grid-inner').offset();
+      },
+      originDelta: function(instrument) {
+				var originOffset = angular.element('.sample-list').offset();
+      	return Math.floor((this.offset(instrument).left - originOffset.left) % this.beatSize);
+      }
+    };
+	}
+]);
