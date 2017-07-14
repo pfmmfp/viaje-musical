63603ca048ae20a4e368d7078f8eff378985b569
Juan Pablo Vercesi <jotapeve@gmail.com>
2017-07-14 15:58:36 -0300

    Regions subregions (#9)
    * Refactor Region/Subregion services
    
    * Refactor instruments controllers
    
    * Change instruments paths
    
    * NEA geo content

--- public/modules/core/services/subregions.client.service.js
+++ public/modules/core/services/subregions.client.service.js
@@ -17,7 +17,84 @@ function(_) {
     "type":"subregion",
     "offsetX":158,
     "offsetY":120
-  }];
+},
+// NEA
+{
+    "id": "",
+    "audio":[],
+    "description":"Son consideradas una de las siete maravillas naturales del Mundo y Patrimonio de la Humanidad. Están compuestas por 275 saltos enmarcados dentro de una selva de vegetación abundante y frondosa con una gran variedad de especies animales. El clima es cálido y las precipitaciones abundantes. Algunos de los animales que las habitan son el yacaré, el coatí, serpientes y mariposas.",
+    "name":"Cataratas del Iguazú",
+    "code": "cataratas-iguazu",
+    "pic":"Cataratas.jpg",
+    "pics":[
+        "Cataratas.jpg",
+      "Cataratas1.jpg"
+    ],
+    "type":"subregion",
+    "offsetX":338,
+    "offsetY":140
+},
+{
+    "id": "",
+    "audio":[],
+    "description":"Se extiende por toda la provincia de Misiones y el noroeste de Corrientes. Se caracteriza por su suelo colorado rico en hierro y la frondosa vegetación de la selva subtropical, rica en especies animales y vegetales. Su clima es cálido y húmedo.",
+    "name":"Meseta Misionera",
+    "code": "meseta-misionera",
+    "pic":"Meseta_misionera.jpg",
+    "pics":[
+        "Meseta_misionera.jpg",
+      "Meseta_misionera_1.jpg"
+    ],
+    "type":"subregion",
+    "offsetX":270,
+    "offsetY":175
+},
+{
+    "id": "",
+    "audio":[],
+    "description":"Iberá significa “agua brillante” en guaraní. Los esteros están formados por lagunas y zonas pantanosas que se encuentran en el centro de la provincia de Corrientes y cuentan con una gran diversidad de especies vegetales y fauna autóctona, tanto de animales acuáticos como terrestres y aves. En la zona hay abundantes lluvias y altas temperaturas.",
+    "name":"Esteros del Iberá",
+    "code": "esteros-ibera",
+    "pic":"Esteros.jpg",
+    "pics":[
+        "Esteros.jpg",
+      "Esteros_1.jpg"
+    ],
+    "type":"subregion",
+    "offsetY":247,
+    "offsetX":185
+},
+{
+    "id": "",
+    "audio":[],
+    "description":"Es una zona ubicada entre el Chaco Húmedo y la Selva de las Yungas en el NOA. Se caracteriza por su clima cálido y seco. El relieve es de llanura y la vegetación se adapta a la sequía del lugar, encontramos: bosques secos como formación principal, y también sabanas y pastizales.",
+    "name":"Chaco Seco",
+    "code": "chaco-seco",
+    "pic":"Chaco_seco.jpg",
+    "pics":[
+        "Chaco_seco.jpg",
+      "Chaco_seco_1.jpg"
+    ],
+    "type":"subregion",
+    "offsetX":50,
+    "offsetY":110
+},
+{
+    "id": "",
+    "audio":[],
+    "description":"Ocupa el oeste de Chaco y Formosa, Noroeste de Corrientes y Norte de Santa Fé. El clima es cálido y húmedo, pero con precipitaciones menores a las de la selva misionera. Sus principales características son su relieve deprimido y los bosques de quebracho colorado y blanco.",
+    "name":"Chaco Húmedo",
+    "code": "chaco-humedo",
+    "pic":"Chaco_húmedo.jpg",
+    "pics":[
+        "Chaco_húmedo.jpg",
+      "Chaco_húmedo_1.jpg"
+    ],
+    "type":"subregion",
+    "offsetX":238,
+    "offsetY":140
+}
+];
   Subregions.byName = function(name) {
     return _.findWhere(this, { name: name });
   };
