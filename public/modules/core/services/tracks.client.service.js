2987a4b702701824433d79a19ed9070aeb45e29a
Pablo Morra <morrapablo@scvsoft.com>
2017-07-28 12:06:01 -0300

    Replaced some zamba samples and updated the milonga sample composition (#20)
    Also added shuffling of samples

--- public/modules/core/services/tracks.client.service.js
+++ public/modules/core/services/tracks.client.service.js
@@ -3,14 +3,24 @@
 
 angular.module('composer').factory('Tracks', ['_', 'TracksNOA', 'TracksNEA', 'TracksPam', 'TracksSP',
 function(_, TracksNOA, TracksNEA, TracksPam, TracksSP) {
-
+  
+  function shuffle() { 
+    return Math.random() - 0.5;
+  }
+  
+  _.each([TracksNOA, TracksNEA, TracksPam, TracksSP], function(regionTracks) {
+    _.each(regionTracks, function(instruments) {
+      instruments.samples.sort(shuffle);
+    });
+  });
+  
   var Tracks = {
     noa: { tracks: TracksNOA, bpm: 96 },
     nea: { tracks: TracksNEA, bpm: 100 },
     pampa: { tracks: TracksPam, bpm: 100 },
     cuyo: { tracks: [], bpm: 100 },
     patagonia: { tracks: [], bpm: 100 },
-    sierras: { tracks: TracksSP, bpm: 70 },
+    sierras: { tracks: TracksSP, bpm: 72 },
   };
 
   return Tracks;
